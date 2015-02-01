a09845e45371746359c30e9804541e96c97456b9
mnotti <markusnotti@MacBook-Pro.local>
2015-01-31 20:56:54 -0800

    Initial Commit

---
+++ Ladr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ladr
+//
+//  Created by Markus Notti on 1/31/15.
+//  Copyright (c) 2015 Markus Notti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
